c9492bef461ee70b906857341edbe9b464d122bc
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-08-05 18:52:21 +0300

    Ui update & auth integrations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{headerShown: false}}/>
     </Stack>
   );
 }
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,23 +1,47 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import { useFonts } from "expo-font";
-import Colors from './../constants/Colors.ts'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { useFonts } from "expo-font"
 import React from 'react'
+import { useRouter } from 'expo-router'
 
 export default function Login() {
+  const router = useRouter();
+
   return (
+
     <View>
       <Image source={require('./../assets/images/Login.jpg')} 
       style={{
         width: '100%',
-        height: 450 }}>
+        height: 520,
+            }}>
         </Image>
 
 
         <View style={styles.container}>
            <Text style={{
-           fontSize:28,
+           fontSize: 30,
            fontFamily: 'outfit-bold', 
-           textAlign: 'center'}}> Traveling Planner</Text>
+           textAlign: 'center',
+           marginTop: 20
+           }}> Travel Pal</Text>
+
+           <Text style={{
+            fontFamily: 'outfit',
+            fontSize: 17,
+            textAlign: 'center',
+            color: '#7d7d7d',
+            marginTop: 20
+           }}>
+            Stay connected with real-time Ai - integrated travel alerts and other features, ensuring you can enjoy your adventures
+           </Text>
+
+           <TouchableOpacity  style={styles.button} onPress={()=>router.push('auth/sign-in')}>
+            <Text style={{color: '#fff', 
+              textAlign: 'center', 
+              fontFamily: 'outfit', 
+              fontSize: 17}}>
+                Get Started</Text>
+           </TouchableOpacity>
         </View>
     </View>
   )
@@ -25,11 +49,17 @@ export default function Login() {
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: Colors.WHITE,
-    marginTop:-20,
+    backgroundColor: '#fff',
+    marginTop: -20,
     height: '100%',
-    borderTopRightRadius:30,
-    borderTopLeftRadius:30,
-    padding:15
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    padding: 25
+  }, 
+  button: {
+    borderRadius: 99,
+    backgroundColor: '#000',
+    padding: 15,
+    marginTop: '25%'
   }
 })
