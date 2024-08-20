cfad98d832b5752407393aedc1e3c4a69dd00d15
Ibrahim Rayamah <165764319+iBz-04@users.noreply.github.com>
2024-08-20 21:00:31 +0300

    Update FirebaseConfig.js

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCy4hNLFPtpaodvHwG8deet6PFvqnIpg70",
+  apiKey: "API_KEY_HERE",
   authDomain: "my-apps-410e7.firebaseapp.com",
   projectId: "my-apps-410e7",
   storageBucket: "my-apps-410e7.appspot.com",
